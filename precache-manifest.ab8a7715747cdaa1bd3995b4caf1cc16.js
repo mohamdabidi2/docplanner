cfd45a6a7e2b3e3b54d53de3786dd211e0576b31
mohamdabidi2 <45004605+mohamdabidi2@users.noreply.github.com>
2018-12-21 15:36:43 +0100

    Updates

---
+++ precache-manifest.ab8a7715747cdaa1bd3995b4caf1cc16.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ebfcc0e9b08c3ca753ec",
+    "url": "/docplanner-react/static/js/runtime~main.ebfcc0e9.js"
+  },
+  {
+    "revision": "661dac07721cbab39d66",
+    "url": "/docplanner-react/static/js/main.661dac07.chunk.js"
+  },
+  {
+    "revision": "35d4967322379881d30c",
+    "url": "/docplanner-react/static/js/1.35d49673.chunk.js"
+  },
+  {
+    "revision": "661dac07721cbab39d66",
+    "url": "/docplanner-react/static/css/main.2c5ff363.chunk.css"
+  },
+  {
+    "revision": "22d63fe59097c7ee40416dd264171ee9",
+    "url": "/docplanner-react/index.html"
+  }
+];
